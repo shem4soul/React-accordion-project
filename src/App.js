fc56e3ea5b015ebb360aae03d43d87ed37176b2c
Shittu Emmanuel <shem4soul@gmail.com>
2026-08-04 17:28:42 +0100

    fix a bug

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import "./styles.css";
 
 const faqs = [
@@ -28,26 +27,19 @@ function Accordion({ data }) {
   return (
     <div className="accordion">
       {data.map((el, i) => (
-        <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
+        <AccordionItem title={el.title} text={el.text} num={i} />
       ))}
     </div>
   );
 }
 
 function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
-
-  function handleToggle() {
-    setIsOpen((isOpen) => !isOpen);
-  }
-
   return (
-    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+    <div className="item">
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
-      <p className="icon">{isOpen ? "-" : "+"}</p>
-
-      {isOpen && <div className="content-box">{text}</div>}
+      <p className="icon">-</p>
+      <div className="content-box">{text}</div>
     </div>
   );
 }
